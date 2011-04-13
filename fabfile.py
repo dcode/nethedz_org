5ae641350ebb4440c50a78ef1bf579025778c68b
Derek Ditch <derek.ditch@gmail.com>
2011-04-13 10:35:17 -0500

    Added some posts and started porting to the new hyde.

--- fabfile.py
+++ fabfile.py
@@ -2,31 +2,41 @@ from fabric.api import *
 import os
 import fabric.contrib.project as project
 
-PROD = 'nethedz@nethedz.webfactional.com'
+PROD = ['nethedz@nethedz.webfactional.com',]
 DEST_PATH = '/home/nethedz/webapps/nethedz_org/html'
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
 
 def clean():
-    local('rm -rf ./deploy')
-    local('find . -name "*.pyc" -delete')
+    """ Go to the root directory """
+    with lcd( ROOT_PATH ):
+        """ Remove generated files """
+        local('rm -rf ./deploy')
+        """ Remove temporary files """
+        local('find . \( -iname \*.pyc -o -name \*~ -o -name .DS_Store \) -delete')
 
 def generate():
-    local('hyde -g -s .')
+    """ Go to the root directory """
+    with lcd( ROOT_PATH ):
+        local('hyde -g -s .')
 
 def regen():
     clean()
     generate()
 
 def serve():
-    local('hyde -w -s . -k')
+    """ Go to the root directory """
+    with lcd( ROOT_PATH ):
+        local('hyde -w -s . -k')
 
 def reserve():
     regen()
     serve()
 
 def smush():
-    local('smusher ./media/images')
+    """ Go to the root directory """
+    with lcd( ROOT_PATH ):
+        local('smusher ./media/images')
 
 @hosts(PROD)
 def publish():
